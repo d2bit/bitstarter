5ad9f835cc9cfd298525fdf24fd318e6bb39fcb6
David BF <burretfresket@gmail.com>
2013-07-07 07:01:49 +0200

    Modified web.js to read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+//fs.openSync('index.html','r');
+var buf = new Buffer(256);
+fs.readSync('index.html', buf);
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+    response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
